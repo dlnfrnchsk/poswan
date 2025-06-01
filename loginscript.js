d2f1230a57a08556961f76457059459d4ea5f247
dlnfrnchsk <manguino.aedieleenfrancheska@gmail.com>
2025-06-01 10:29:15 +0000

    firstcom

---
+++ loginscript.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {        
+        container.classList.remove('active');
+});
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent form from submitting
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
+
+    const validUsername = "pufferpish";
+    const validPasswords = ["pufferpish1213", "pufferfish1215"];
+
+    if (username === validUsername && validPasswords.includes(password)) {
+        alert("Login successful!");
+    } else {
+        alert("Invalid username or password.");
+    }
+});
